4921d35e7563c95b74d2b67bd9445b2b1512916d
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-11-11 17:38:57 +0200

    update helmet

--- src/helmet.js
+++ src/helmet.js
@@ -23,5 +23,33 @@ export const HelmetMetaTags = ({ url, description, title, quote, image }) => (
     <meta content="image/*" property="og:image:type" />
     <meta property="og:description" content={description} />
     <meta data-rh="true" property="fb:app_id" content="542599432471018"></meta>
+
+    <meta property="og:url" content="https://github.com" />
+    <meta property="og:site_name" content="GitHub" />
+    <meta property="og:title" content="Build software better, together" />
+    <meta property="og:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="1200" />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="620" />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-octocat.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="620" />
+    <meta property="twitter:site" content="github" />
+    <meta property="twitter:site:id" content="13334762" />
+    <meta property="twitter:creator" content="github" />
+    <meta property="twitter:creator:id" content="13334762" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content="GitHub" />
+    <meta property="twitter:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
+    <meta property="twitter:image:src" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
+    <meta property="twitter:image:width" content="1200" />
+    <meta property="twitter:image:height" content="1200" />
+    <meta name="page-subject" content="GitHub" />
   </Helmet>
 )
