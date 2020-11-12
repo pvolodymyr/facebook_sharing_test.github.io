0c3ba488e96412ed79f966878f205eba5256bb1c
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-11-12 11:33:00 +0200

    update

--- src/helmet.js
+++ src/helmet.js
@@ -19,26 +19,19 @@ export const HelmetMetaTags = () => (
     <meta property="og:title" content="GitHub: Where the world builds software" />
     <meta property="og:url" content="https://github.com/" />
     <meta property="og:description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and feat..." />
-    <meta name="request-id" content="EB48:FED9:1A624B:23F0FB:5FAAA304" data-pjax-transient="true" />
-    <meta name="html-safe-nonce" content="43aa7131606ac8e0c7d8908403554f5b0a9706f494fe8f00bac532d0b370a7d8" data-pjax-transient="true" />
-    <meta name="visitor-payload" content="eyJyZWZlcnJlciI6IiIsInJlcXVlc3RfaWQiOiJFQjQ4OkZFRDk6MUE2MjRCOjIzRjBGQjo1RkFBQTMwNCIsInZpc2l0b3JfaWQiOiI4NjE5MjQwODQzMzg3NzczNzAxIiwicmVnaW9uX2VkZ2UiOiJmcmEiLCJyZWdpb25fcmVuZGVyIjoiZnJhIn0=" data-pjax-transient="true" />
-    <meta name="visitor-hmac" content="d1b00581602b20b70c6287c79bb049f7ea269292867233e34a7f096533e14537" data-pjax-transient="true" />
-    <meta name="cookie-consent-required" content="true" data-pjax-transient="true" />
+    <meta name="request-id" content="EB48:FED9:1A624B:23F0FB:5FAAA304" />
+    <meta name="html-safe-nonce" content="43aa7131606ac8e0c7d8908403554f5b0a9706f494fe8f00bac532d0b370a7d8" />
+    <meta name="visitor-payload" content="eyJyZWZlcnJlciI6IiIsInJlcXVlc3RfaWQiOiJFQjQ4OkZFRDk6MUE2MjRCOjIzRjBGQjo1RkFBQTMwNCIsInZpc2l0b3JfaWQiOiI4NjE5MjQwODQzMzg3NzczNzAxIiwicmVnaW9uX2VkZ2UiOiJmcmEiLCJyZWdpb25fcmVuZGVyIjoiZnJhIn0=" />
+    <meta name="visitor-hmac" content="d1b00581602b20b70c6287c79bb049f7ea269292867233e34a7f096533e14537" />
     <meta name="page-subject" content="GitHub" />
-    <meta name="github-keyboard-shortcuts" content="dashboards" data-pjax-transient="true" />
-    <meta name="selected-link" value="/" data-pjax-transient="" />
+    <meta name="github-keyboard-shortcuts" content="dashboards" />
+    <meta name="selected-link" value="/" />
     <meta name="google-site-verification" content="c1kuD-K2HIVF635lypcsWPoD4kilo5-jA_wBFyT4uMY" />
     <meta name="google-site-verification" content="KT5gs8h0wvaagLKAVWq8bbeNwnZZK1r1XQysX3xurLU" />
     <meta name="google-site-verification" content="ZzhVyEFwb7w3e0-uOTltm8Jsck2F5StVihD0exw2fsA" />
     <meta name="google-site-verification" content="GXs5KoUUkNCoaAZn7wPN-t01Pywp9M3sEjnt_3_ZWPc" />
-    <meta name="octolytics-host" content="collector.githubapp.com" />
-    <meta name="octolytics-app-id" content="github" />
-    <meta name="octolytics-event-url" content="https://collector.githubapp.com/github-external/browser_event" />
-    <meta name="octolytics-dimension-ga_id" content="" class="js-octo-ga-id" />
-    <meta class="js-ga-set" name="dimension10" content="Responsive" data-pjax-transient="" />
-    <meta class="js-ga-set" name="dimension1" content="Logged Out" />
+
     <meta name="hostname" content="github.com" />
-    <meta name="user-login" content="" />
     <meta name="expected-hostname" content="github.com" />
     <meta name="enabled-features" content="MARKETPLACE_PENDING_INSTALLATIONS" />
     <meta name="homepage-version-ga-dimension" content="dimension11" />
@@ -49,5 +42,10 @@ export const HelmetMetaTags = () => (
     <meta name="browser-errors-url" content="https://api.github.com/_private/browser/errors" />
     <meta name="browser-optimizely-client-errors-url" content="https://api.github.com/_private/browser/optimizely_client/errors" />
     <meta name="theme-color" content="#1e2327" />
+
+    <link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000" />
+    <link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://github.githubassets.com/favicons/favicon-dark.png" />
+    <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon-dark.svg" />
+    <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />
   </Helmet>
 )
