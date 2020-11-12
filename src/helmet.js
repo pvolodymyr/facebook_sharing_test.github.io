3257665f325e585c962bec55a6530e719ce53228
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-11-12 11:11:50 +0200

    update helmet

--- src/helmet.js
+++ src/helmet.js
@@ -3,31 +3,51 @@ import { Helmet } from 'react-helmet'
 
 export const HelmetMetaTags = () => (
   <Helmet>
-    <meta property="og:url" content="https://github.com" />
-    <meta property="og:site_name" content="GitHub" />
-    <meta property="og:title" content="Build software better, together" />
-    <meta property="og:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
-    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="1200" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and features, power your CI/CD and DevOps workflows, and secure code before you commit it." />
+    <meta property="fb:app_id" content="1401488693436528" />
+    <meta name="apple-itunes-app" content="app-id=1477376905" />
+    <meta name="twitter:image:src" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta name="twitter:site" content="@github" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="GitHub: Where the world builds software" />
+    <meta name="twitter:description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and feat..." />
     <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta property="og:site_name" content="GitHub" />
+    <meta property="og:type" content="object" />
+    <meta property="og:title" content="GitHub: Where the world builds software" />
+    <meta property="og:url" content="https://github.com/" />
+    <meta property="og:description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and feat..." />
+    <meta name="request-id" content="EB48:FED9:1A624B:23F0FB:5FAAA304" data-pjax-transient="true" />
+    <meta name="html-safe-nonce" content="43aa7131606ac8e0c7d8908403554f5b0a9706f494fe8f00bac532d0b370a7d8" data-pjax-transient="true" />
+    <meta name="visitor-payload" content="eyJyZWZlcnJlciI6IiIsInJlcXVlc3RfaWQiOiJFQjQ4OkZFRDk6MUE2MjRCOjIzRjBGQjo1RkFBQTMwNCIsInZpc2l0b3JfaWQiOiI4NjE5MjQwODQzMzg3NzczNzAxIiwicmVnaW9uX2VkZ2UiOiJmcmEiLCJyZWdpb25fcmVuZGVyIjoiZnJhIn0=" data-pjax-transient="true" />
+    <meta name="visitor-hmac" content="d1b00581602b20b70c6287c79bb049f7ea269292867233e34a7f096533e14537" data-pjax-transient="true" />
+    <meta name="cookie-consent-required" content="true" data-pjax-transient="true" />
+    <meta name="page-subject" content="GitHub" />
+    <meta name="github-keyboard-shortcuts" content="dashboards" data-pjax-transient="true" />
+    <meta name="selected-link" value="/" data-pjax-transient="" />
+    <meta name="google-site-verification" content="c1kuD-K2HIVF635lypcsWPoD4kilo5-jA_wBFyT4uMY" />
+    <meta name="google-site-verification" content="KT5gs8h0wvaagLKAVWq8bbeNwnZZK1r1XQysX3xurLU" />
+    <meta name="google-site-verification" content="ZzhVyEFwb7w3e0-uOTltm8Jsck2F5StVihD0exw2fsA" />
+    <meta name="google-site-verification" content="GXs5KoUUkNCoaAZn7wPN-t01Pywp9M3sEjnt_3_ZWPc" />
+    <meta name="octolytics-host" content="collector.githubapp.com" />
+    <meta name="octolytics-app-id" content="github" />
+    <meta name="octolytics-event-url" content="https://collector.githubapp.com/github-external/browser_event" />
+    <meta name="octolytics-dimension-ga_id" content="" class="js-octo-ga-id" />
+    <meta class="js-ga-set" name="dimension10" content="Responsive" data-pjax-transient="" />
+    <meta class="js-ga-set" name="dimension1" content="Logged Out" />
+    <meta name="hostname" content="github.com" />
+    <meta name="user-login" content="" />
+    <meta name="expected-hostname" content="github.com" />
+    <meta name="enabled-features" content="MARKETPLACE_PENDING_INSTALLATIONS" />
+    <meta name="homepage-version-ga-dimension" content="dimension11" />
     <meta property="og:image:type" content="image/png" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="620" />
-    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-octocat.png" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="620" />
-    <meta property="twitter:site" content="github" />
-    <meta property="twitter:site:id" content="13334762" />
-    <meta property="twitter:creator" content="github" />
-    <meta property="twitter:creator:id" content="13334762" />
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:title" content="GitHub" />
-    <meta property="twitter:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
-    <meta property="twitter:image:src" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
-    <meta property="twitter:image:width" content="1200" />
-    <meta property="twitter:image:height" content="1200" />
+    <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats" />
+    <meta name="browser-errors-url" content="https://api.github.com/_private/browser/errors" />
+    <meta name="browser-optimizely-client-errors-url" content="https://api.github.com/_private/browser/optimizely_client/errors" />
+    <meta name="theme-color" content="#1e2327" />
   </Helmet>
 )
