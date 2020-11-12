dd0203cb7de25dedb610ae1da7ccce2111f23392
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-11-12 11:04:37 +0200

    update helmet

--- src/helmet.js
+++ src/helmet.js
@@ -3,10 +3,31 @@ import { Helmet } from 'react-helmet'
 
 export const HelmetMetaTags = () => (
   <Helmet>
-    <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="When Great Minds Don’t Think Alike" />
-    <meta property="og:description" content="How much does culture influence creative thinking?" />
-    <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+    <meta property="og:url" content="https://github.com" />
+    <meta property="og:site_name" content="GitHub" />
+    <meta property="og:title" content="Build software better, together" />
+    <meta property="og:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="1200" />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="620" />
+    <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-octocat.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="620" />
+    <meta property="twitter:site" content="github" />
+    <meta property="twitter:site:id" content="13334762" />
+    <meta property="twitter:creator" content="github" />
+    <meta property="twitter:creator:id" content="13334762" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content="GitHub" />
+    <meta property="twitter:description" content="GitHub is where people build software. More than 50 million people use GitHub to discover, fork, and contribute to over 100 million projects." />
+    <meta property="twitter:image:src" content="https://github.githubassets.com/images/modules/open_graph/github-logo.png" />
+    <meta property="twitter:image:width" content="1200" />
+    <meta property="twitter:image:height" content="1200" />
   </Helmet>
 )
