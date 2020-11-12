13ec3722c61907cb4eac63476d47fa1534788d61
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-11-12 12:36:08 +0200

    UPDATE

--- src/helmet.js
+++ src/helmet.js
@@ -5,47 +5,33 @@ export const HelmetMetaTags = () => (
   <Helmet>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <meta name="description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and features, power your CI/CD and DevOps workflows, and secure code before you commit it." />
-    <meta property="fb:app_id" content="1401488693436528" />
-    <meta name="apple-itunes-app" content="app-id=1477376905" />
+  
+    
+    
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@nytimes" />
     <meta name="twitter:creator" content="@SarahMaslinNir" />
-    <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral" />
+    <meta name="twitter:title" content="GitHub: Where the world builds software" />
     <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here." />
     <meta name="twitter:image" content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg" />
     <meta property="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="620" />
     <meta property="og:site_name" content="GitHub" />
     <meta property="og:type" content="object" />
     <meta property="og:title" content="GitHub: Where the world builds software" />
     <meta property="og:url" content="https://github.com/" />
     <meta property="og:description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and feat..." />
-    <meta name="request-id" content="EB48:FED9:1A624B:23F0FB:5FAAA304" />
-    <meta name="html-safe-nonce" content="43aa7131606ac8e0c7d8908403554f5b0a9706f494fe8f00bac532d0b370a7d8" />
-    <meta name="visitor-payload" content="eyJyZWZlcnJlciI6IiIsInJlcXVlc3RfaWQiOiJFQjQ4OkZFRDk6MUE2MjRCOjIzRjBGQjo1RkFBQTMwNCIsInZpc2l0b3JfaWQiOiI4NjE5MjQwODQzMzg3NzczNzAxIiwicmVnaW9uX2VkZ2UiOiJmcmEiLCJyZWdpb25fcmVuZGVyIjoiZnJhIn0=" />
-    <meta name="visitor-hmac" content="d1b00581602b20b70c6287c79bb049f7ea269292867233e34a7f096533e14537" />
-    <meta name="page-subject" content="GitHub" />
-    <meta name="github-keyboard-shortcuts" content="dashboards" />
-    <meta name="selected-link" value="/" />
-    <meta name="google-site-verification" content="c1kuD-K2HIVF635lypcsWPoD4kilo5-jA_wBFyT4uMY" />
-    <meta name="google-site-verification" content="KT5gs8h0wvaagLKAVWq8bbeNwnZZK1r1XQysX3xurLU" />
-    <meta name="google-site-verification" content="ZzhVyEFwb7w3e0-uOTltm8Jsck2F5StVihD0exw2fsA" />
-    <meta name="google-site-verification" content="GXs5KoUUkNCoaAZn7wPN-t01Pywp9M3sEjnt_3_ZWPc" />
-
-    <meta name="hostname" content="github.com" />
-    <meta name="expected-hostname" content="github.com" />
-    <meta name="enabled-features" content="MARKETPLACE_PENDING_INSTALLATIONS" />
-    <meta name="homepage-version-ga-dimension" content="dimension11" />
-    <meta property="og:image:type" content="image/png" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="620" />
-    <meta name="browser-stats-url" content="https://api.github.com/_private/browser/stats" />
-    <meta name="browser-errors-url" content="https://api.github.com/_private/browser/errors" />
-    <meta name="browser-optimizely-client-errors-url" content="https://api.github.com/_private/browser/optimizely_client/errors" />
-    <meta name="theme-color" content="#1e2327" />
-
-    <link rel="mask-icon" href="https://github.githubassets.com/pinned-octocat.svg" color="#000000" />
-    <link rel="alternate icon" class="js-site-favicon" type="image/png" href="https://github.githubassets.com/favicons/favicon-dark.png" />
-    <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="https://github.githubassets.com/favicons/favicon-dark.svg" />
+   
+    <meta name="description" content="GitHub is where over 50 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and features, power your CI/CD and DevOps workflows, and secure code before you commit it." />
+    <meta name="og:image" content="https://github.githubassets.com/images/modules/open_graph/github-mark.png" />
+    <meta name="og:image:type" content="image/png" />
+    <meta name="og:image:width" content="1200" />
+    <meta name="og:image:height" content="620" />
+    <meta name="og:site_name" content="GitHub" />
+    <meta name="og:type" content="object" />
+    <meta name="og:title" content="GitHub: Where the world builds software" />
+    <meta name="og:url" content="https://github.com/" />
   </Helmet>
 )
